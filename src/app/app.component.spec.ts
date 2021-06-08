b3d38b07753d8a8a68bbabcaaa86e06e2f420f26
Eduardo Queiroz <jose.queirozedu@ctis.com.br>
2021-06-08 14:52:46 -0300

    build

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'shrimpApp'`, () => {
+  it(`should have as title 'ecamarao'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('shrimpApp');
+    expect(app.title).toEqual('ecamarao');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('shrimpApp app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ecamarao app is running!');
   });
 });
